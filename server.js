46491ae2795709ff0a4d8c733dbfdfeaa28cd7d3
nikita-schurov22 <tyup3637@mail.ru>
2024-10-28 19:53:05 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Установка маршрута для отдачи index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Определите API для вычисления квадратного корня
+app.get('/api/sqrt', (req, res) => {
+    const number = parseFloat(req.query.number);
+    if (isNaN(number)) {
+        return res.status(400).json({ error: 'Некорректное число' });
+    }
+    const result = Math.sqrt(number);
+    res.json({ result });
+});
+
+// Запуск сервера
+app.listen(port, () => {
+    console.log(`Сервер работает на http://localhost:${port}`);
+});
